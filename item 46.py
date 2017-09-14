0015714de15125ed03b459e39d92ee445b7b7474
Samme Qandil <Saqandil@gmail.com>
2017-09-14 12:48:16 -0700

    Add files via upload

---
+++ item 46.py
@@ -0,0 +1,13 @@
+# trying out the range functions
+for i in range(4):
+    print (i)
+
+print ('this is just to seperate these things')
+
+for j in range(3,-1,-1): #for later samme,
+    print (j)#          remember (first number,  second number, and this is the increment)
+
+print ('this is just to seperate these things')
+
+for s in range(8,0,-2):
+    print(s)
